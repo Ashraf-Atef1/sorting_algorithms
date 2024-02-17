e3f2f82bc12243271718f8870af3015d5d17c3fe
Ashraf Atef <ashraf.programer@gmail.com>
2024-02-17 19:33:36 +0200

    task

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,5 +1,7 @@
 #include "sort.h"
 
+void quick_sort_rec(int *array, size_t size, int start, int end);
+
 /**
  * quick_sort - Sorting an array with a quick_sort algo.
  * @array: An array to be sorted
@@ -8,26 +10,16 @@
  */
 void quick_sort(int *array, size_t size)
 {
-	size_t i, j, value;
-	int tmp, swaped = 1;
-
-	for (i = 0; swaped && i < size; i++)
-	{
-		value = i;
-		for (j = i; j < size; j++)
-		{
-			if (array[value] > array[j])
-				value = j;
-		}
-		if (value != i)
-		{
-			tmp = array[i];
-			array[i] = array[value];
-			array[value] = tmp;
-			print_array(array, size);
-		}
-	}
+	quick_sort_rec(array, size, 0, (int)size - 1);
 }
+/**
+ * swap - Swap two elemnts in an array the print the updated array.
+ * @array: An array to be sorted
+ * @size: The size of the array
+ * @first: first element index
+ * @second: second element index
+ * Ashraf Atef
+ */
 void swap(int *array, size_t size, size_t first, size_t second)
 {
 	int tmp;
@@ -40,21 +32,40 @@ void swap(int *array, size_t size, size_t first, size_t second)
 		print_array(array, size);
 	}
 }
-size_t partition_sort(int *array, size_t size, size_t start, size_t end)
+/**
+ * partition_sort - Dived an array into a two partitions.
+ * @array: An array to be sorted
+ * @size: The size of the array
+ * @start: start index
+ * @end: end index
+ * Return: pivit index
+ * Ashraf Atef
+ */
+int partition_sort(int *array, size_t size, int start, int end)
 {
 	int pivit = array[end];
-	size_t p_index = start, i;
+	int p_index = start, i;
+
 	for (i = start; i < end; i++)
 	{
 		if (array[i] <= pivit)
 		{
 			swap(array, size, i, p_index);
+			p_index++;
 		}
 	}
 	swap(array, size, end, p_index);
 	return (p_index);
 }
-void quick_sort_rec(int *array, size_t size, size_t start, size_t end)
+/**
+ * quick_sort_rec - Sorting an array with a quick_sort algo.
+ * @array: An array to be sorted
+ * @size: The size of the array
+ * @start: start index
+ * @end: end index
+ * Ashraf Atef
+ */
+void quick_sort_rec(int *array, size_t size, int start, int end)
 {
 	size_t pivit;
 
