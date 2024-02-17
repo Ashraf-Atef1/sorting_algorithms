0ba0c4a286a681d4af63ffb9458508e29d129fdd
Ashraf Atef <ashraf.programer@gmail.com>
2024-02-17 18:38:08 +0200

    task

---
+++ 100-shell_sort.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+
+/**
+ * shell_sort - Sorting an array with a shell_sort algo.
+ * @array: An array to be sorted
+ * @size: The size of the array
+ * Ashraf Atef
+ */
+void shell_sort(int *array, size_t size)
+{
+	size_t gap = 1, j, i;
+	int tmp;
+
+	while (gap * 3 + 1 < size)
+	{
+		gap = gap * 3 + 1;
+	}
+	for (; gap; gap /= 3)
+	{
+		for (i = gap; i < size; i++)
+		{
+			for (j = i; j >= gap && array[j - gap] > array[j]; j -= gap)
+			{
+				tmp = array[j];
+				array[j] = array[j - gap];
+				array[j - gap] = tmp;
+			}
+		}
+		print_array(array, size);
+	}
+}
