4e9c50c008fe9f951f6cc7c7d7dc588a7960bd71
Ashraf Atef <ashraf.programer@gmail.com>
2024-02-17 20:50:32 +0200

    task

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -26,24 +26,36 @@ void cocktail_sort_list(listint_t **list)
 {
 	listint_t *l_head = list ? *list : NULL;
 	listint_t *cur_node = NULL;
+	int s_size = 0, l_size = 0, i = 0;
 
 	while (l_head != NULL)
 	{
 		cur_node = l_head;
-		while (cur_node)
+		while (cur_node && (!l_size || s_size + i < l_size))
 		{
 			if (cur_node->next && cur_node->n > cur_node->next->n)
+			{
 				swap(list, cur_node, cur_node->next);
+				continue;
+			}
 			if (!cur_node->next)
 				break;
 			cur_node = cur_node->next;
+			s_size++;
 		}
+		l_size = l_size ? l_size : s_size;
 		while (cur_node)
 		{
 			if (cur_node->prev && cur_node->prev->n > cur_node->n)
+			{
 				swap(list, cur_node->prev, cur_node);
+				continue;
+			}
 			cur_node = cur_node->prev;
+			if (!s_size--)
+				break;
 		}
 		l_head = l_head->next;
+		i++;
 	}
 }
