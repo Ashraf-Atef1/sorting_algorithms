3e77adeb5fb33c44923c86e09cfe246682e05aee
Ashraf Atef <ashraf.programer@gmail.com>
2024-02-18 02:03:25 +0200

    task

---
+++ 102-counting_sort.c
@@ -0,0 +1,30 @@
+#include "sort.h"
+
+/**
+ * counting_sort - Sorting an array with a counting_sort algo.
+ * @array: An array to be sorted
+ * @size: The size of the array
+ * Ashraf Atef
+ */
+void counting_sort(int *array, size_t size)
+{
+	size_t i;
+	int *count_array, *sorted_array, max = 0;
+
+	for (i = 0; i < size; i++)
+		if (array[i] > max)
+			max = array[i];
+
+	count_array = (int *)calloc(sizeof(int), max + 1);
+	sorted_array = (int *)calloc(sizeof(int), size);
+	for (i = 0; i < size; i++)
+		count_array[array[i]]++;
+	for (i = 1; i <= (size_t)max; i++)
+		count_array[i] += count_array[i - 1];
+	for (i = 0; i < size; i++)
+		sorted_array[count_array[array[i]]] = array[i];
+	for (i = 0; i < size; i++)
+		array[i] = sorted_array[i];
+	print_array(count_array, max + 1);
+	free(count_array), free(sorted_array);
+}
